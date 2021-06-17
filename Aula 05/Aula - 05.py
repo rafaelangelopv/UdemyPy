9ad66b799ebb0ea3524443a1dc529cc53c0b0e54
Rafael Vale <rafaelangelopv@gmail.com>
2021-06-17 14:57:59 -0300

    2st commit

---
+++ Aula 05/Aula - 05.py
@@ -0,0 +1,47 @@
+"""
+Tipos de dados
+
+str - string - textos 'assim' "assim"
+int - inteiro - 123456 0 -10 -20 -50 -60 -1500 1500
+float - real/ponto flutuante - 10.50 1.5 -10.10 -50.93 0.0
+bool - booleano/lógico - True/False 10==10
+"""
+print('Luiz', type('Luiz'))
+
+print(10, type(10))
+print('10', type('10'))
+
+print(25.23, type(25.23))
+print('25.23', type('25.23'))
+
+print(10==10, type(10==10))
+
+print('L'=='L',type('L'=='L'))
+print('L'=='l',type('L'=='l'))
+
+print(bool(''))
+
+#TypeQuesting
+
+#print('Rafael', type('Rafael'), bool('Rafael'))
+#print('10', type('10'), type(int('10')))
+#print('10', type('10'), type(10))
+
+# print('Rafael', int('Rafael'))
+# print('Rafael', int('10.5'))
+
+# print('Rafael', float('10.5'))
+# print('Rafael', float('10'))
+
+# print(10+10)
+# print('10'+'10')
+# print('10'+10)
+
+#String Nome
+# print('Rafael', type('Rafael'))
+#Idade int
+# print('30', type(int('30')))
+#Altura float
+# print(1.68, type(1.68))
+#É maior de idade 10>20
+# print(30 > 18, type(30>18))
