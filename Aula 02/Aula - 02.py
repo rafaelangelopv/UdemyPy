304b6353eaf52429cc5ed8fa4a68db99a14d2fad
Rafael Vale <rafaelangelopv@gmail.com>
2021-06-17 14:33:26 -0300

    1st commit

---
+++ Aula 02/Aula - 02.py
@@ -0,0 +1,19 @@
+
+print(123456)
+print('Rafael', 'Angelo', 'Outra coisa')
+print('Rafael', 'Marcella', sep='-', end=' // ')
+print('Rafael', 'e', 'Marcella', sep='-')
+
+print('Dados')
+
+# sep='' // separador
+# end='' // final da linha de código, o padrão é uma quebra de linha
+
+"""
+842.176.070-18
+
+Gerar CPF
+"""
+print('CPF', end= ':')
+print('824', '176', '070',sep='.', end='-')
+print('18')
---
+++ Aula 03/Aula - 03.py
@@ -0,0 +1,13 @@
+"""
+str - string
+"""
+print('alguma coisa - aspas simples')
+print("alguma coisa - aspas duplas")
+print(123456)
+
+print('Essa é uma "string" (str)')
+print("Essa é uma 'string' (str)")
+print("Esse é meu \"texto\" (str).")
+print('Esse é meu \'texto\' (str).')
+print('Esse é meu \n (str).')
+print(r'Esse é meu \n (str).') ## não executar nada na linha usa r♦
