77cc9da5e7f9d993e6ea7906756d7b3b2d3cf5ec
Rafael Vale <51975462+rafaelangelopv@users.noreply.github.com>
2021-07-17 01:07:03 -0300

    9st Commit

--- Section - 02/Aula - 18.py
+++ Section - 02/Aula - 18.py
@@ -14,3 +14,16 @@ https://docs.python.org/3/library/functions.html (tipos built-in)
 https://docs.python.org/3/library/stdtypes.html (funções built-in)
 
 """
+# Positivos [012345678]
+texto = 'Python s2'
+
+print(texto[2])  # t
+print(texto[8])  # 2
+print(texto[6])  # ""
+print(texto[9])  # "Erro - Fora do Range"
+
+# Negativos -[987654321]
+texto = 'Python s2'
+
+url = 'www.google.com.br/'
+print(url[:-1])  # retira a barra no final da URL
